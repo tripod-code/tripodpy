a33c41f61d8be18dbf013f1681e9565863063e57
Sebastian Stammler <sebastian.stammler@gmail.com>
2022-01-28 15:15:30 +0100

    Added module for standard library

---
+++ twopoppy/std/__init__.py
@@ -0,0 +1,10 @@
+'''Package containing standard functions that can be used in simulations.'''
+
+from twopoppy.std import dust
+from twopoppy.std import sim
+
+
+__all__ = [
+    "dust",
+    "sim"
+]
---
+++ twopoppy/std/dust.py
@@ -0,0 +1,1 @@
+'''Module containing standard functions for the dust.'''
