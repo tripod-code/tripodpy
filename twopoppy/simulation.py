d08c4df196ac29b75018342c18f4906d6248c495
Sandro Paetzold <98543228+paetzolds@users.noreply.github.com>
2022-02-23 14:38:21 +0100

    Delete further unnecessary fields in sim.ini

--- twopoppy/simulation.py
+++ twopoppy/simulation.py
@@ -10,7 +10,7 @@ from . import std
 
 class Simulation(dp.Simulation):
     """The main simulation class for running dust evolution simulations.
-    `twopoppy.Simulation`` is a child of ``dustpy.Simulation``, 
+    `twopoppy.Simulation`` is a child of ``dustpy.Simulation``,
     which is in turn a child of ``simframe.Frame``.
     For setting simple initial conditions use ``Simulation.ini``,
     For making the simulation grids use ``Simulation.makegrids()``,
@@ -71,6 +71,9 @@ class Simulation(dp.Simulation):
         self.dust.S.updater = updtordr
 
         # Deleting Fields that are not needed
+        del(self.ini.grid.Nmbpd)
+        del(self.ini.grid.mmin)
+        del(self.ini.grid.mmax)
         del(self.dust.coagulation)
         del(self.dust.kernel)
         del(self.grid.m)
